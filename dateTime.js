bda0f255bc9d19f6393ecf06639b2b1c8ba8f3d0
TAQ2000 <129269653+TAQ2000@users.noreply.github.com>
2023-09-19 21:14:22 +0600

    Update dateTime.js

--- dateTime.js
+++ dateTime.js
@@ -13,12 +13,20 @@ function showTime() {
     const time = date.toLocaleString('en-US', options);
     return `Time: ${time}`;
 }
+function timefy(datatime) {
+    const temp_time = new Date(datatime);
+    const time = new Date(temp_time.getTime() - 12 * 60 * 1000);
+    const options = { hour: 'numeric', minute: 'numeric', hour12: true };
+    const fullTime = time.toLocaleTimeString('en-US', options);
+    return fullTime;
+}
 
 {
     var dt = new Date();
     let flag = 1;
     if (dt.getHours() > 17) {
         flag = 0;
+        dt.setDate(dt.getDate() + 1);
     }
     const date = showDate();
     const time = showTime();
@@ -30,5 +38,29 @@ function showTime() {
         const timeElement = document.createElement('p')
         timeElement.textContent = time;
         container.appendChild(timeElement);
+    }
+    const tdsp = [];
+    const data = JSON.parse(localStorage.getItem('data'));
+    for(let i = 30; i < data.content.length; i++) {
+        const date = new Date(data.content[i][0]);
+        if ((date.getFullYear() === dt.getFullYear()) && (date.getMonth() === dt.getMonth()) && (date.getDate() === dt.getDate())){
+            tdsp.push(data.content[i]);
+        }
+    }
+    if(tdsp.length) {
+        const container2 = document.createElement('div');
+        container2.classList.add('container');
+        const p = document.createElement('p');
+        p.classList.add('mystyle');
+        p.textContent = "Today's Special !!!";
+        container2.appendChild(p);
+        const ul = document.createElement('ul');
+        for(let i = 0; i < tdsp.length; i++) {
+            const li = document.createElement('li');
+            li.textContent = timefy(tdsp[i][1]) + ' \u2192 ' + `${tdsp[i][2]}`;
+            ul.appendChild(li);
+        }
+        container2.appendChild(ul);
+        document.body.appendChild(container2);
     }
 }
