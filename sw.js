e691c8554f69e9ab8a553ee22f7d3ec884931479
TAQ2000 <129269653+TAQ2000@users.noreply.github.com>
2023-10-31 10:49:37 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 self.addEventListener('install', evt => {
-    console.log('Service worker installed', evt);
+    // console.log('Service worker installed', evt);
 });
 
 self.addEventListener('activate', evt => {
-    console.log('Service worker activated', evt);
+    // console.log('Service worker activated', evt);
 });
 
 self.addEventListener('fetch', evt => {
-    console.log('fetch', evt);
+    // console.log('fetch', evt);
 });
