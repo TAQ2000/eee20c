46ad40e7b9a25aa4b7435adea789e1fd9648f210
TAQ2000 <129269653+TAQ2000@users.noreply.github.com>
2023-11-01 12:38:06 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js')
-    .then((reg) => console.log('Service worker registered', reg))
-    .catch((err) => console.log('Service worker not registered', err));
+    navigator.serviceWorker.register('sw.js').then().catch();
 }
